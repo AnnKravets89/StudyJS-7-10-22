fdd27a9824eb18c63812ebb6d1d4d42bbdc32649
Anna Kravets <kravetsanna78@gmail.com>
2022-10-07 18:38:12 +0300

    second practice

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const numberOfFilms = prompt("How many films have you seen already?", "");
 
 
+
+
 const personalMovieBd = {
     count: numberOfFilms,
     movies:  {
@@ -12,14 +14,34 @@ const personalMovieBd = {
 
 };
 
-const  a = prompt("One of the films you watched?", "");
-const  b = prompt("How many point do you give it?", "");
-const  c = prompt("One of the films you watched?", "");
-const  d = prompt("How many point do you give it?", "");
+for (let i = 0; i < 2; i++) {
+    const  a = prompt("One of the films you watched?", ""),
+           b = prompt("How many point do you give it?", "");
 
-personalMovieBd.movies[a] = b;
-personalMovieBd.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieBd.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+        
+}
+
+if (personalMovieBd.count < 10) {
+    console.log('You watched quite few films');
+} else if (personalMovieBd.count >= 10 && personalMovieBd.count < 30) {
+    console.log('You are a cool watcher') ;
+} else if (personalMovieBd.count >= 30) {
+    console.log('You are a movieguy!');
+} else {
+    console.log('Wrong answer');
+}
+
 
 console.log(personalMovieBd);
+
+
+
 
 
